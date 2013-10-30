052b59fa77aef16588a911ebb753e5b6a612f0a9
lesslucifer <less.lucifer@yahoo.com.vn>
2013-10-30 11:09:04 +0700

    complete welcome layout

--- oakclubbuild/TutorialViewController.m
+++ oakclubbuild/TutorialViewController.m
@@ -12,6 +12,7 @@
 
 @interface TutorialViewController ()
 {
+    UIPageControl *pageControl;
 }
 @end
 
@@ -31,26 +32,36 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
+    UIButton *closeButton = [[UIButton alloc] init];
+    [closeButton setBackgroundImage:[UIImage imageNamed:@"tutorial_cancel.png"] forState:UIControlStateNormal];
+    [closeButton sizeToFit];
+    closeButton.frame = CGRectMake(self.view.frame.size.width - closeButton.frame.size.width - 10, 10, closeButton.frame.size.width, closeButton.frame.size.height);
+    [closeButton addTarget:self action:@selector(closeButtonTouched:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:closeButton];
+    
+    pageControl = [[UIPageControl alloc] init];
+    pageControl.numberOfPages = 3;
+    [pageControl sizeToFit];
+    NSLog(@"old frame %f-%f-%f-%f", pageControl.frame.origin.x, pageControl.frame.origin.y, pageControl.frame.size.width, pageControl.frame.size.height);
+    pageControl.frame = CGRectMake((self.view.frame.size.width - pageControl.frame.size.width) / 2, self.view.frame.size.height - pageControl.frame.size.height - 10, pageControl.frame.size.width, pageControl.frame.size.height);
+    NSLog(@"new frame %f-%f-%f-%f", pageControl.frame.origin.x, pageControl.frame.origin.y, pageControl.frame.size.width, pageControl.frame.size.height);
+    pageControl.currentPage = 0;
+    [self.view addSubview:pageControl];
+    
     NSArray* pageImages = [NSArray arrayWithObjects:
-                           [UIImage imageNamed:@"intropage_snap.png"],
-                           [UIImage imageNamed:@"intropage_chat.png"],
-                           [UIImage imageNamed:@"intropage_match.png"],
+                           [UIImage imageNamed:@"tutorial_snapshots.png"],
+                           [UIImage imageNamed:@"tutorial_chat.png"],
+                           [UIImage imageNamed:@"tutorial_options.png"],
                            nil];
-    CycleScrollView *cycle = [[CycleScrollView alloc] initWithFrame:CGRectMake(0, 0, 320, 455)
+    CycleScrollView *cycle = [[CycleScrollView alloc] initWithFrame:CGRectMake(0, 0, 320, 460)
                                                      cycleDirection:CycleDirectionLandscape
                                                            pictures:pageImages];
     
     cycle.delegate = self;
     [self.view addSubview:cycle];
     [self.view sendSubviewToBack:cycle];
-    
-    UIButton *closeButton = [[UIButton alloc] init];
-    [closeButton setBackgroundImage:[UIImage imageNamed:@"cancel-btn.png"] forState:UIControlStateNormal];
-    [closeButton sizeToFit];
-    closeButton.frame = CGRectMake(self.view.frame.size.width - closeButton.frame.size.width, 0, closeButton.frame.size.width, closeButton.frame.size.height);
-    [closeButton addTarget:self action:@selector(closeButtonTouched:) forControlEvents:UIControlEventTouchUpInside];
-    
-    [self.view addSubview:closeButton];
+    [self.view bringSubviewToFront:pageControl];
+    [pageControl setHidden:NO];
 }
 
 - (void)didReceiveMemoryWarning
@@ -71,7 +82,7 @@
 }
 
 - (void)cycleScrollViewDelegate:(CycleScrollView *)cycleScrollView didScrollImageView:(int)index{
-    //pageControl.currentPage = index-1;
+    pageControl.currentPage = index-1;
 }
 
 -(void)closeButtonTouched:(id)sender
