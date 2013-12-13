b03d5dfc5b1221ddaa046f6430df500948c6b71d
ntnhuy <ntnhuy294@gmail.com>
2013-12-13 13:34:15 +0700

    alert view

---
+++ oakclubbuild/UIAlert.h
@@ -0,0 +1,13 @@
+//
+//  UIAlert.h
+//  OakClub
+//
+//  Created by To Huy on 12/13/13.
+//  Copyright (c) 2013 VanLuu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlert : UIAlertView
+-(void) setButton: (NSString*) text withFrame: (CGRect) frame;
+@end
