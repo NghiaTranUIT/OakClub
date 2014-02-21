8bb34c572f52ff58f5c9960bfe6b1f58a0b6989b
lesslucifer <less.lucifer@yahoo.com.vn>
2014-02-21 17:06:21 +0700

    + remove never post on facebook in loginView

--- oakclubbuild/UAModelPanelEx.m
+++ oakclubbuild/UAModelPanelEx.m
@@ -28,7 +28,7 @@
         NSString *content4String = [@"Other users can't contact you unless you've already been matched" localize];
         NSString *content5String = [@"Your location will NEVER be shown to other users" localize];
         
-                                
+        NSArray *contents = @[content1String, content3String, content4String, content5String];
         
         UIImage *contentImg = [UIImage imageNamed:@"screen"];
         self.backgroundColor = [UIColor colorWithPatternImage:contentImg];
@@ -52,108 +52,18 @@
         [titleLabel setFont:FONT_HELVETICANEUE_LIGHT(30)];
         
         CGPoint startPoint = CGPointMake((self.frame.size.width - 210)/2, 167);
-        CGFloat textWidth = 250;
-        CGFloat spacingParagraph=2;
-        if(IS_HEIGHT_GTE_568)
-            spacingParagraph = 10;
-        CGFloat deltaIconPosition = 2;
+        for (int i = 0; i < contents.count; ++i) {
+            startPoint = [self addContentLabel:contents[i] atPosition:startPoint];
+        }
         
-        UIImageView *image1 = [[UIImageView alloc] init];
-        UILabel *content1Label = [[UILabel alloc] init];
-        [content1Label setLineBreakMode:NSLineBreakByWordWrapping];
-        [content1Label setNumberOfLines: 0];
-        [content1Label setText: content1String];
-        [content1Label setFont: FONT_HELVETICANEUE_LIGHT(11)];
-        //Set frame according to string
-        CGSize size = [content1Label.text sizeWithFont:content1Label.font
-                             constrainedToSize:CGSizeMake(textWidth, MAXFLOAT)
-                                 lineBreakMode:NSLineBreakByWordWrapping];
-        [content1Label setFrame:CGRectMake(startPoint.x , startPoint.y , size.width , size.height)];
-        [image1 setImage: [UIImage imageNamed: @"bullet.png"]];
-        [image1 setFrame: CGRectMake((self.frame.size.width - 240)/2, content1Label.frame.origin.y + deltaIconPosition, 11, 11)];
-        
-        UIImageView *image2 = [[UIImageView alloc] init];
-        UILabel *content2Label = [[UILabel alloc] init];
-        [content2Label setLineBreakMode:NSLineBreakByWordWrapping];
-        [content2Label setNumberOfLines: 0];
-        [content2Label setText: content2String];
-        [content2Label setFont: FONT_HELVETICANEUE_LIGHT(11)];
-        CGSize size2 = [content2Label.text sizeWithFont:content2Label.font
-                                     constrainedToSize:CGSizeMake(textWidth, MAXFLOAT)
-                                         lineBreakMode:NSLineBreakByWordWrapping];
-        [content2Label setFrame:CGRectMake(startPoint.x , content1Label.frame.origin.y + content1Label.frame.size.height + spacingParagraph, size2.width , size2.height)];
-        [image2 setImage: [UIImage imageNamed: @"bullet.png"]];
-        [image2 setFrame: CGRectMake((self.frame.size.width - 240)/2, content2Label.frame.origin.y+ deltaIconPosition, 11, 11)];
-        
-        UIImageView *image3 = [[UIImageView alloc] init];
-        UILabel *content3Label = [[UILabel alloc] init];
-        [content3Label setLineBreakMode:NSLineBreakByWordWrapping];
-        [content3Label setNumberOfLines: 0];
-        [content3Label setText: content3String];
-        [content3Label setFont: FONT_HELVETICANEUE_LIGHT(11)];
-        CGSize size3 = [content3Label.text sizeWithFont:content3Label.font
-                                      constrainedToSize:CGSizeMake(textWidth, MAXFLOAT)
-                                          lineBreakMode:NSLineBreakByWordWrapping];
-        [content3Label setFrame:CGRectMake(startPoint.x , content2Label.frame.origin.y + content2Label.frame.size.height + spacingParagraph, size3.width , size3.height)];
-        [image3 setImage: [UIImage imageNamed: @"bullet.png"]];
-        [image3 setFrame: CGRectMake((self.frame.size.width - 240)/2, content3Label.frame.origin.y+ deltaIconPosition, 11, 11)];
-        
-        UIImageView *image4 = [[UIImageView alloc] init];
-        UILabel *content4Label = [[UILabel alloc] init];
-        [content4Label setLineBreakMode:NSLineBreakByWordWrapping];
-        [content4Label setNumberOfLines: 0];
-        [content4Label setText: content4String];
-        [content4Label setFont: FONT_HELVETICANEUE_LIGHT(11)];
-        CGSize size4 = [content4Label.text sizeWithFont:content4Label.font
-                                      constrainedToSize:CGSizeMake(textWidth, MAXFLOAT)
-                                          lineBreakMode:NSLineBreakByWordWrapping];
-        [content4Label setFrame:CGRectMake(startPoint.x , content3Label.frame.origin.y + content3Label.frame.size.height + spacingParagraph, size4.width , size4.height)];
-        [image4 setImage: [UIImage imageNamed: @"bullet.png"]];
-        [image4 setFrame: CGRectMake((self.frame.size.width - 240)/2, content4Label.frame.origin.y+ deltaIconPosition, 11, 11)];
-        
-        UIImageView *image5 = [[UIImageView alloc] init];
-        UILabel *content5Label = [[UILabel alloc] init];
-        [content5Label setLineBreakMode:NSLineBreakByWordWrapping];
-        [content5Label setNumberOfLines: 0];
-        [content5Label setText: content5String];
-        [content5Label setFont: FONT_HELVETICANEUE_LIGHT(11)];
-        CGSize size5 = [content5Label.text sizeWithFont:content5Label.font
-                                      constrainedToSize:CGSizeMake(textWidth, MAXFLOAT)
-                                          lineBreakMode:NSLineBreakByWordWrapping];
-        [content5Label setFrame:CGRectMake(startPoint.x , content4Label.frame.origin.y + content4Label.frame.size.height + spacingParagraph, size5.width , size5.height)];
-        [image5 setImage: [UIImage imageNamed: @"bullet.png"]];
-        [image5 setFrame: CGRectMake((self.frame.size.width - 240)/2, content5Label.frame.origin.y+ deltaIconPosition, 11, 11)];
-       
         [signInButton addTarget:self action:@selector(loginTouched:) forControlEvents:UIControlEventTouchUpInside];
         [signInButton setTitle:@"Connect Privately" forState:UIControlStateNormal];
         [signInButton.titleLabel setFont:[UIFont systemFontOfSize:15]];
         
-//        if(IS_OS_7_OR_LATER)
-//        {
-//            [image1 setFrame: CGRectMake((self.frame.size.width - 240)/2, 175, 11, 11)];
-//            [image2 setFrame: CGRectMake((self.frame.size.width - 240)/2, 207, 11, 11)];
-//            [image3 setFrame: CGRectMake((self.frame.size.width - 240)/2, 238, 11, 11)];
-//            [image4 setFrame: CGRectMake((self.frame.size.width - 240)/2, 270, 11, 11)];
-//            [image5 setFrame: CGRectMake((self.frame.size.width - 240)/2, 301, 11, 11)];
-//        }
-        
         [self addSubview: titleLabel];
-        [self addSubview: content1Label];
-        [self addSubview: content2Label];
-        [self addSubview: content3Label];
-        [self addSubview: content4Label];
-        [self addSubview: content5Label];
-        [self addSubview: image1];
-        [self addSubview: image2];
-        [self addSubview: image3];
-        [self addSubview: image4];
-        [self addSubview: image5];
-        
         [self addSubview:signInButton];
         
         [self setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:[@"Info_background" localize]]]];
-        
-        [self localizeAllViews];
         
         loginPage = login;
     }
@@ -161,6 +71,34 @@
     [self localizeAllViews];
     
     return self;
+}
+
+-(CGPoint)addContentLabel:(NSString *)content atPosition:(CGPoint)pos
+{
+    static float deltaIconPosition = 2;
+    float spacingParagraph = (IS_HEIGHT_GTE_568)?10:2;
+    static float maxWidth = 250;
+    
+    UIImage *bulletImage = [UIImage imageNamed: @"bullet.png"];
+    UIImageView *bulletImgView = [[UIImageView alloc] init];
+    UILabel *contentLbl = [[UILabel alloc] init];
+    [contentLbl setLineBreakMode:NSLineBreakByWordWrapping];
+    [contentLbl setNumberOfLines:0];
+    [contentLbl setText:content];
+    [contentLbl setFont:FONT_HELVETICANEUE_LIGHT(11)];
+    CGSize textSize = [content sizeWithFont:contentLbl.font
+                                  constrainedToSize:CGSizeMake(maxWidth, MAXFLOAT)
+                                      lineBreakMode:NSLineBreakByWordWrapping];
+    [contentLbl setFrame:CGRectMake(pos.x, pos.y, textSize.width , textSize.height)];
+    [bulletImgView setImage:bulletImage];
+    [bulletImgView setFrame:CGRectMake((self.frame.size.width - 240)/2, pos.y + deltaIconPosition, 11, 11)];
+    
+    pos.y += contentLbl.frame.size.height + spacingParagraph;
+    
+    [self addSubview:bulletImgView];
+    [self addSubview:contentLbl];
+    
+    return pos;
 }
 
 -(void)loginTouched:(id)sender
