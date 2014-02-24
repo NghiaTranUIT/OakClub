b1eefaa3e4def2b3d34db2ef281cc0770fddc828
letiendungbk <letiendungbk@yahoo.com.vn>
2014-02-24 19:08:17 +0700

    - localization for User Verification (English, VietNam, Turkish, Arabic, Indonesia)

--- oakclubbuild/AppDelegate.m
+++ oakclubbuild/AppDelegate.m
@@ -210,8 +210,7 @@ static const int ddLogLevel = LOG_LEVEL_INFO;
     
     // Initialize tracker.
     id<GAITracker> tracker = [[GAI sharedInstance] trackerWithTrackingId:kGoolgeTrackingId];
-    [tracker set:kGAIScreenName
-           value:@"Home Screen"];
+    
     return YES;
 }
 
@@ -2044,4 +2043,5 @@ static const int ddLogLevel = LOG_LEVEL_INFO;
     }];
 
 }
+
 @end
